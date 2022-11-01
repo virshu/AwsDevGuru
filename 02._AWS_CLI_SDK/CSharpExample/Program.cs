acd83bfabdc9ed13e5c07bb1c8d8dc86c889f46d
virshu <felix@rabinovich.org>
2022-10-31 17:06:33 -0700

    Chapter 2 C# Example

---
+++ 02._AWS_CLI_SDK/CSharpExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.Runtime;
+
+AWSCredentials awsCredentials = FallbackCredentialsFactory.GetCredentials();
+
+AmazonEC2Client client = new(awsCredentials);
+DescribeInstancesRequest request = new();
+
+DescribeInstancesResponse response = await client.DescribeInstancesAsync(request);
+
+Console.WriteLine($"Total Reservations: {response.Reservations.Count}");
+if (response.Reservations.Count == 0) return;
+
+foreach (Reservation reservation in response.Reservations)
+{
+    foreach ((Instance? instance, string? name) in from instance in reservation.Instances
+         from string name in instance.Tags.Select(tag => tag.Key == "Name" ? tag.Value : "Anonymous")
+         select (instance, name))
+    {
+        Console.WriteLine($"Name: {name}\t\t\tPub. IP: {instance.PublicIpAddress}");
+    }
+}
