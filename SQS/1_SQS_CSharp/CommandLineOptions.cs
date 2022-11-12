d9940d61ac05f89956db65e9627942dc208d8a1c
virshu <felix@rabinovich.org>
2022-11-11 18:43:55 -0800

    Create SQS queue

---
+++ SQS/1_SQS_CSharp/CommandLineOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace SQS;
+
+internal class CommandLineOptions
+{
+    [Option('p', "profile", Required = false, 
+        HelpText = "Profile name to use for credentials. Leave blank to use default or IAM Role credentials")]
+    public string? Profile { get;  set; }
+
+}
--- SQS/1_SQS_CSharp/Program.cs
+++ SQS/1_SQS_CSharp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using CommandLine;
+using SQS;
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+// Get credentials
+AWSCredentials awsCredentials = FallbackCredentialsFactory.GetCredentials();
+
+bool finish = false;
+Parser.Default.ParseArguments<CommandLineOptions>(args)
+    .WithParsed(co =>
+    {
+        if (co.Profile is null) return;
+        // required, or govcloud won't work
+        AWSConfigs.AWSProfileName = co.Profile;
+        SharedCredentialsFile sharedfile = new();
+        if (!sharedfile.TryGetProfile(co.Profile, out CredentialProfile profile))
+        {
+            Console.WriteLine($"Could not find profile {co.Profile}");
+            finish = true;
+            return;
+        }
+
+        AWSCredentialsFactory.TryGetAWSCredentials(profile, sharedfile, out awsCredentials);
+    })
+    .WithNotParsed(_ => finish = true);
+if (finish) return;
+
+using StreamReader r = new("settings.json");
+string json = r.ReadToEnd();
+
+JsonElement source = JsonSerializer.Deserialize<JsonElement>(json)!;
+
+AmazonSQSClient sqs = new(awsCredentials);
+CreateQueueRequest request = new()
+{
+    QueueName = source.GetProperty("queueName").GetString()
+};
+string? QueueName = source.GetProperty("queueName").GetString();
+CreateQueueResponse? result = await sqs.CreateQueueAsync(QueueName);
+Console.WriteLine(result is null ? "Error" : $"Sucess: {result.QueueUrl}");
+
